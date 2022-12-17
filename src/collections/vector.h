27cd877cba32e0032e42676d4974c804adb5d619
daystzh <daystzh@163.com>
2022-12-18 00:13:20 +0800

    vector copy construct and move construct

--- src/collections/vector.h
+++ src/collections/vector.h
@@ -19,6 +19,78 @@ class vector {
   Allocate allocator_;
 
  public:
+  explicit vector(size_t size, T &&val) : size_(size), cap_(size) {
+    data_ = new T[cap_];
+    for (size_t i = 0; i < size_; i++) {
+      data_[i] = val;
+    }
+  }
+
+  vector() : size_(0), cap_(0) {}
+
+  vector(const vector &other) {
+    size_ = other.size_;
+    cap_ = other.cap_;
+    data_ = allocator_.allocate(cap_);
+    for (int i = 0; i < size_; i++) {
+      data_[i] = other[i];
+    }
+  }
+
+  vector(vector &&other) {
+    size_ = other.size_;
+    cap_ = other.cap_;
+    data_ = other.data_;
+    other.data_ = nullptr;
+    other.~vector();
+  }
+
+  ~vector() {
+    for (size_t i = 0; i < size_; i++) {
+      data_[i].~T();
+    }
+    if (cap_ > 0) {
+      allocator_.deallocate(data_, cap_);
+    }
+  }
+
+  T &operator[](size_t index) { return data_[index]; }
+
+  const T &operator[](size_t index) const { return data_[index]; }
+
+  bool operator==(const vector &other) {
+    if (size_ != other.size()) {
+      return false;
+    }
+
+    for (size_t i = 0; i < size_; i++) {
+      if (!(data_[i] == other[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  T &back() { return data_[size_ - 1]; }
+
+  T &front() { return data_[0]; }
+
+  bool operator!=(const vector &other) { return !this->operator==(other); }
+
+  void swap(vector &other) {
+    using nstd::swap;
+    swap(data_, other.data_);
+    swap(size_, other.size_);
+    swap(cap_, other.cap_);
+  }
+
+  size_t max_size() {
+    //      return 100;
+    return size_t(-1) / sizeof(T);
+  }
+
+  bool empty() { return size_ == 0; }
+
   void push_back(const T &val) {
     if (size_ == cap_) {
       resize();
@@ -69,61 +141,6 @@ class vector {
     size_--;
     data_[size_].~T();
   }
-
-  ~vector() {
-    for (size_t i = 0; i < size_; i++) {
-      data_[i].~T();
-    }
-    if (cap_ > 0) {
-      allocator_.deallocate(data_, cap_);
-    }
-  }
-
-  explicit vector(size_t size, T &&val) : size_(size), cap_(size) {
-    data_ = new T[cap_];
-    for (size_t i = 0; i < size_; i++) {
-      data_[i] = val;
-    }
-  }
-
-  vector() : size_(0), cap_(0) {}
-
-  T &operator[](size_t index) { return data_[index]; }
-
-  const T &operator[](size_t index) const { return data_[index]; }
-
-  bool operator==(const vector &other) {
-    if (size_ != other.size()) {
-      return false;
-    }
-
-    for (size_t i = 0; i < size_; i++) {
-      if (!(data_[i] == other[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  T &back() { return data_[size_ - 1]; }
-
-  T &front() { return data_[0]; }
-
-  bool operator!=(const vector &other) { return !this->operator==(other); }
-
-  void swap(vector &other) {
-    using nstd::swap;
-    swap(data_, other.data_);
-    swap(size_, other.size_);
-    swap(cap_, other.cap_);
-  }
-
-  size_t max_size() {
-    //      return 100;
-    return size_t(-1) / sizeof(T);
-  }
-
-  bool empty() { return size_ == 0; }
 
  private:
   void resize() {
